a6f2567c0acc7e6ff2e23b696b7ec1c0ca296273
Kevin Chen <kevin@Kevinui-MacBookPro.local>
2024-02-24 21:46:04 +1100

    edit index.js and added app.js, controllers and models folder

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+mongoose.connect(mongoUrl)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error('error connecting to MongoDB', error.message))
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
