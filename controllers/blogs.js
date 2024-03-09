81a4159bafab380fe0692fcb6db2c1c1bbe0b2ac
Kevin Chen <kevin@Kevinui-MacBookPro.local>
2024-03-10 03:30:46 +1100

    added login router and token based authentication for adding new blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
@@ -9,21 +19,26 @@ blogsRouter.get('/', async (request, response) => {
   
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
-    const user = await User.findById(body.userId)
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes || 0,
-      user: user.id
-    })
+  
+    try {
+      const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+      if(!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+      }
+      const user = await User.findById(decodedToken.id)
 
-    try { 
+      const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0,
+        user: user.id
+      })
       const savedBlog = await blog.save()
       user.blogs = user.blogs.concat(savedBlog._id)
       await user.save()
       response.status(201).json(savedBlog)
-    }
+      }
     catch(exception) {
       next(exception)
     }
